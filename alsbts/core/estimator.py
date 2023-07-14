ae59cfd0cd7991a875c127a30a0d1669f51b2dcc
bela127 <bhb127@outlook.de>
2023-07-14 12:45:21 +0200

    estimator update to new subscriber names and interface

--- alsbts/core/estimator.py
+++ alsbts/core/estimator.py
@@ -4,13 +4,14 @@ from typing import TYPE_CHECKING
 from dataclasses import dataclass
 
 from alts.core.experiment_module import ExperimentModule
-from alts.core.subscriber import ExperimentSubscriber, ResultSubscriber
+from alts.core.subscriber import ExpModSubscriber, ResultDataSubscriber
 
 if TYPE_CHECKING:
     from nptyping import  NDArray, Number, Shape
+    from alts.core.subscribable import Subscribable
 
 @dataclass
-class Estimator(ExperimentSubscriber, ResultSubscriber):
+class Estimator(ExperimentModule, ExpModSubscriber, ResultDataSubscriber):
 
     def __post_init__(self):
         super().__post_init__()
@@ -24,13 +25,13 @@ class Estimator(ExperimentSubscriber, ResultSubscriber):
     def train(self) -> None:
         pass
 
-    def result_update(self):
-        super().result_update()
+    def result_update(self, subscription: Subscribable):
+        super().result_update(subscription)
         self.train()
         
 
-    def experiment_update(self):
-        super().experiment_update()
+    def experiment_update(self, subscription: Subscribable):
+        super().experiment_update(subscription)
         times = self.exp_modules.data_pools.stream.last_queries
         vars = self.exp_modules.data_pools.stream.last_results
         queries = self.exp_modules.oracles.process.last
