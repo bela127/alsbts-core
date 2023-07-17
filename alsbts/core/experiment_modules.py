ddc878d3944f4d1c9522ecb9382319c2121385a8
bela127 <bhb127@outlook.de>
2023-07-17 17:31:17 +0200

    Move initial_query_sampler to ExperimentModules

--- alsbts/core/experiment_modules.py
+++ alsbts/core/experiment_modules.py
@@ -3,8 +3,10 @@ from typing import TYPE_CHECKING
 
 from dataclasses import dataclass
 
-from alts.core.experiment_modules import ExperimentModules
+from alts.core.experiment_modules import InitQueryExperimentModules
 from alts.core.configuration import init
+from alsbts.modules.query.query_sampler import StreamQuerySampler
+
 
 if TYPE_CHECKING:
     from alsbts.core.estimator import Estimator
@@ -12,11 +14,15 @@ if TYPE_CHECKING:
     from alts.core.oracle.oracle import Oracle
     from alts.core.configuration import Required
     from typing_extensions import Self
+    from alts.core.query.query_sampler import QuerySampler
+
 
 
 @dataclass
-class StreamExperiment(ExperimentModules):
+class StreamExperiment(InitQueryExperimentModules):
     estimator: Estimator = init()
+
+    initial_query_sampler: QuerySampler = init(default_factory=StreamQuerySampler)
 
     def __post_init__(self):
         super().__post_init__()
